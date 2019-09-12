094c53f37a051c73ed5998fed1de1e2a1bdda858
Khlebnikov Igor <i7.khlebnikov@gmail.com>
2019-09-12 14:35:47 -0700

    Initial Commit

---
+++ L1_KhlebnikovIgorUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L1_KhlebnikovIgorUI
+//
+//  Created by Igor on 9/12/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
