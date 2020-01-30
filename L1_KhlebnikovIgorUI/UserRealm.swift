e76866caae1fc069bcca4a7609fdc98c41762fed
Igor <i7.khlebnikov@gmail.com>
2020-01-30 16:30:31 +0400

    Домашнее задание к уроку №6

--- L1_KhlebnikovIgorUI/UserRealm.swift
+++ L1_KhlebnikovIgorUI/UserRealm.swift
@@ -6,42 +6,46 @@
 //  Copyright © 2020 Igor. All rights reserved.
 //
 
-import Foundation
+
 import RealmSwift
 
 class UserRealm: Object{
-        @objc dynamic var id = 0
-        @objc dynamic var firstName = ""
-        @objc dynamic var lastName = ""
-        @objc dynamic var photo100 = ""
+    @objc dynamic var id = 0
+    @objc dynamic var firstName = ""
+    @objc dynamic var lastName = ""
+    @objc dynamic var photo100 = ""
     var city: CityRealm?
 }
 class CityRealm: Object {
     @objc dynamic var  id = 0
     @objc dynamic var  title = ""
     
-
-
     
-
+    
+    
+    
 }
 
 class UsersRepositoryRealm{
     
-    func addUser(firstName: String, lastName: String, photo100: String/*, city: CityRealm*/){
-        let realm = try? Realm()
-        let newUser = UserRealm()
-        newUser.id = 0
-        newUser.firstName = firstName
-        newUser.lastName = lastName
-        newUser.photo100 = photo100
-//        newUser.city = city
-
-        try? realm?.write {
-            realm?.add(newUser)
+    func addUser(user: User){
+        do{
+            let realm = try Realm()
+            try realm.write {
+            let userRealm = UserRealm()
+            userRealm.id = user.id
+            userRealm.firstName = user.firstName
+            userRealm.lastName = user.lastName
+            userRealm.photo100 = user.avatarPath ?? ""
+            //        newUser.city = city
+            realm.add(userRealm)
+            }
+        }
+        catch{
+            print(error)
         }
     }
-     
+    
     func getUser(id: Int)->UserRealm?{
         let realm = try! Realm()
         return realm.objects(UserRealm.self).filter("id == %@", id).first
---
+++ L1_KhlebnikovIgorUI/UsersRepositoryRealm.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  L1_KhlebnikovIgorUI
+//
+//  Created by Игорь Хлебников on 30.01.2020.
+//  Copyright © 2020 Igor. All rights reserved.
+//
+
+import Foundation
